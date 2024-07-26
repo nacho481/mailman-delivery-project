bbe8387db83e064980e31879a6f8178736949901
Ignacio-Manuel Atilano <50087750+nacho481@users.noreply.github.com>
2024-07-26 16:10:14 -0700

    I finallyyyy fixed the departure time for each package.

--- Package.py
+++ Package.py
@@ -126,6 +126,9 @@ class Package:
                 f"Departure time: {departure_time_str:<{departure_time_width}} "
                 f"{self.m_status:<{status_width}} "
                 f"{truck_info:<{truck_width}}")
+        # return (f"{self.m_ID}, {address}, {city}, {state}, {intermediate_zip}, "
+        #         f"{self.m_deadline}, {self.m_weight}, Delivery time: {delivery_time_str}, "
+        #         f"Departure time: {departure_time_str}, {self.m_status}, {truck_info}")
 
     def m_update_status(self, time):
         """
